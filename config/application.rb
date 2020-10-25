48788481dd53009687e39b7a0f84282c1cf72654
allenle <letianji@qq.com>
2020-10-26 02:57:20 +0800

    调教

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module RailsBootstrap
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
